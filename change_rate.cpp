43ec7ecdbd6eef63f7a8f02155b99dce9db210c1
1F659162 <45314100+1F659162@users.noreply.github.com>
2019-07-03 09:11:47 +0700

    Add files via upload

---
+++ change_rate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () { short money;
+
+cout << "Input thai Baths : ";
+cin >> money;
+cout << "****Exchange Rate****" << endl;
+cout << money << " Baths is " << money/30.7125 << " dollars" << endl; 
+cout << money << " Baths is " << money/35.0631 << " Euro" << endl; 
+
+return(0);
+}
